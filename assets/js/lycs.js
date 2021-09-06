35a97e2644615fbdd6688858520e7457907d689a
Gaston Funes <gtfunes@gmail.com>
2021-09-06 20:02:02 -0300

    Updated site

--- assets/js/lycs.js
+++
@@ -1,6 +0,0 @@
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-
-ga('create', 'UA-63528430-1', 'auto');
-ga('require', 'displayfeatures');
-ga('set', 'appName', 'My Website');
-ga('send', 'pageview');
